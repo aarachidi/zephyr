16d809fbf03989e5d94b64ce1704ab7a8bfe779a
aarachidi <rachidi.rhd.97@gmail.com>
2022-02-17 06:16:40 -0800

    add pinmux

---
+++ buildHifive/zephyr/zephyr.dts.d
@@ -0,0 +1,4 @@
+empty_file.o: /home/arachidi/zephyrproject/zephyr/misc/empty_file.c \
+ /home/arachidi/zephyrproject/zephyr/boards/riscv/hifive1/hifive1.dts \
+ /home/arachidi/zephyrproject/zephyr/dts/riscv/riscv32-fe310.dtsi \
+ /home/arachidi/zephyrproject/zephyr/include/dt-bindings/gpio/gpio.h
